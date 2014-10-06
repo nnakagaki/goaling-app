6cd0d954374de29aa3b989244d190999e295b1ea
Norris Nakagaki <norrisnakagaki@gmail.com>
2014-10-06 16:47:07 -0400

    Finished the two-table approach to comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root to: "users#index"
+
   resources :users, only: [:index, :show, :new, :create] do
     resources :goals, only: [:show]
+    resources :user_comments, only: [:create]
   end
-  resources :goals, except: [:index, :new, :show]
+
+  resources :goals, except: [:index, :new, :show] do
+    resources :goal_comments, only: [:create]
+  end
 
   resource :session, only: [:new, :create, :destroy]
 end
